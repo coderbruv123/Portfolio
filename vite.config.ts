22da16f7d038632e36896eaed368eab0ea3c16a8
coderbruv123 <giriprashant516@gmail.com>
2025-05-27 17:15:56 +0545

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/Portfolio/', // ✅ make sure this matches the repo name exactly
+  plugins: [react(),
+    tailwindcss()
+  ],
+
+  base: '/Portfolio/', 
 })
